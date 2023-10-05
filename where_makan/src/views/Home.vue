09a3c28a81dc3916acca7e05a2990a2155a1d327
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-05 19:43:08 +0800

    Update Home.vue

--- where_makan/src/views/Home.vue
+++ where_makan/src/views/Home.vue
@@ -4,7 +4,10 @@
       <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
       <HelloWorld msg="You did it!" />
     </div>
- 
+
+    <div>
+        <button>Nearby Me</button>
+    </div>
 </template>
 
 <script>
