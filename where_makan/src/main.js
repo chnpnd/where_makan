e6b59cb67c493705f69b6ef2bec50d1f8ae610bc
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-05 15:47:25 +0800

    Update main.js

--- where_makan/src/main.js
+++ where_makan/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routers';
 import VueGoogleMaps from '@fawmi/vue-google-maps';
+import VueGeolocation from '@fawmi/vue-browser-geolocation';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
@@ -14,3 +15,4 @@ app.use(VueGoogleMaps, {
         key: 'AIzaSyBCa9b0AaISqJ2I3eidqjG1L_eFzlTOKmw',
     },
 }).mount('#app')
+app.use(VueGeolocation);
