037bc6ae00c7f66f77f73cea3a72b5b6b4b08288
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-05 20:12:01 +0800

    Update Home.vue

--- where_makan/src/views/Home.vue
+++ where_makan/src/views/Home.vue
@@ -6,8 +6,10 @@
     </div>
 
     <div>
-        <button>Nearby Me</button>
+        <h1>Your coordinates:</h1>
+        <p>{{ coordinates.lat }} Latitude, {{ coordinates.lng }} Longitude</p>
     </div>
+    
 </template>
 
 <script>
@@ -20,13 +22,26 @@
     }
   }
 
-import GeoLocation from '@/components/GeoLocation.vue'
-    export default {
-    name: 'GeoLocation',
-    components:{
-        GeoLocation
+ export default {
+      name: 'App',
+      data() {
+        return {
+          coordinates: {
+            lat: 0,
+            lng: 0,
+          }
+      }
+  },
+  //get users coordinates from browser request
+        created() {
+          this.$getLocation(options)
+            .then(coordinates => {
+              console.log(coordinates);
+    });
+          .catch(error => alert(error));
+
     }
-    }
+  }
 </script>
 
   
