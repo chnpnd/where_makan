2201faea4cf4afd71d10e146cf5d6533a8e896b7
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-05 15:31:10 +0800

    Update Geolocation.vue

--- where_makan/src/components/Geolocation.vue
+++ where_makan/src/components/Geolocation.vue
@@ -1,1 +1,23 @@
+<template>
+  <div>
+    <h1>Your coordinates:</h1>
+    <p>{{ coordinates.lat }} Latitude, {{ coordinates.lng }} Longitude</p>
+  </div>
+</template>
 
+<script>
+  export default {
+    data() {
+      return {
+        coordinates: {
+          lat: 0,
+          lng: 0
+        }
+    }
+},
+//get users coordinates from browser request
+  created() {
+
+  }
+    
+</script>
