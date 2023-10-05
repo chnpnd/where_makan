dc97e834f5ce4c1cb492b92a5ad4bd0c01bd4591
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-05 21:42:46 +0800

    Update Home.vue

--- where_makan/src/views/Home.vue
+++ where_makan/src/views/Home.vue
@@ -4,6 +4,9 @@
       <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
       <HelloWorld msg="You did it!" />
     </div>
+
+    <button onclick="getLocation()">Try It</button>
+    <p id="demo"></p>
     
 </template>
 
@@ -16,6 +19,18 @@
       HelloWorld
     }
   }
+    const x = document.getElementById("demo");
+    
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+      }
+    }
+    
+    function showPosition(position) {
+      x.innerHTML = "Latitude: " + position.coords.latitude + 
+      "<br>Longitude: " + position.coords.longitude;
+    }
 </script>
-
-  
