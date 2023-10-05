3513e956e58fc147d01c49a8d1352f2467d07e95
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-05 20:17:48 +0800

    Update Home.vue

--- where_makan/src/views/Home.vue
+++ where_makan/src/views/Home.vue
@@ -4,11 +4,6 @@
       <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
       <HelloWorld msg="You did it!" />
     </div>
-
-    <div>
-        <h1>Your coordinates:</h1>
-        <p>{{ coordinates.lat }} Latitude, {{ coordinates.lng }} Longitude</p>
-    </div>
     
 </template>
 
@@ -19,27 +14,6 @@
     name: 'Home',
     components:{
       HelloWorld
-    }
-  }
-
- export default {
-      name: 'App',
-      data() {
-        return {
-          coordinates: {
-            lat: 0,
-            lng: 0,
-          }
-      }
-  },
-  //get users coordinates from browser request
-        created() {
-          this.$getLocation(options)
-            .then(coordinates => {
-              console.log(coordinates);
-    });
-          .catch(error => alert(error));
-
     }
   }
 </script>
