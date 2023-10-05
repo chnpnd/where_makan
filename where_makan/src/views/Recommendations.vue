61a0b24b0ab4fa08bba0ec1ae780ab39a4d657b3
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-05 16:49:12 +0800

    Update Recommendations.vue

--- where_makan/src/views/Recommendations.vue
+++ where_makan/src/views/Recommendations.vue
@@ -1,1 +1,19 @@
+<template>
 
+    <div class="recommendations">
+      <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
+      <HelloWorld msg="You did it!" />
+    </div>
+ 
+</template>
+
+<script>
+  import GeoLocation from '@/components/GeoLocation.vue'
+
+  export default {
+    name: 'GeoLocation',
+    components:{
+      GeoLocation
+    }
+  }
+</script>
