1f5ec3cdbfb4bb3a47e62c26247d51de735bf4ce
nur athirah <athirahmn.2022@scis.smu.edu.sg>
2023-10-06 01:07:32 +0800

    Home view layout changed

--- where_makan/src/views/Home.vue
+++ where_makan/src/views/Home.vue
@@ -1,19 +1,45 @@
 <template>
-
-    <div class="home">
-      <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
-      <HelloWorld msg="You did it!" />
+  <div>
+    <div>
+      <Header />
     </div>
+    <v-divider></v-divider>
+    <h1 class="homeH1 text-center">Food Nearby You:</h1>
+    <v-divider></v-divider>
+    <!-- None  <576px, sm  ≥576px, md  ≥768px, lg  ≥992px, xl  ≥1200px, xxl  ≥1400px -->
+    <div id="map">
+      <Map />
+    </div>
+    <v-divider></v-divider>
+    <Homecards />
+  </div>
+
+    
  
 </template>
 
 <script>
-  import HelloWorld from '@/components/HelloWorld.vue'
+
+
+
+  import Header from '@/components/Home/HelloWorld.vue';
+  import Map from '@/GoogleMap.vue';
+  import Homecards from '@/components/Home/Homecards.vue';
+
 
   export default {
     name: 'Home',
     components:{
-      HelloWorld
+      Header,
+      Map,
+      Homecards,
     }
   }
 </script>
+<style>
+
+
+
+
+
+</style>
