2694f694dd6796bbe2ec51a6c0c3036861033189
nur athirah <athirahmn.2022@scis.smu.edu.sg>
2023-10-06 01:05:33 +0800

    changed the path names a bit

--- where_makan/src/routers/index.js
+++ where_makan/src/routers/index.js
@@ -1,17 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import App from '../App.vue';
 import Home from '../views/Home.vue';
-import About from '../views/About.vue'
+import Login from '../views/About.vue'
 import Restaurant from '../views/Restaurant/Restaurant.vue'
-import Login from '../GoogleMap.vue'
+import Explore from '../GoogleMap.vue'
 import Analytics from '../views/Analytics.vue';
 
 const routes = [
-    { path: '/', component: Home },
-    { path: '/about', component: About },
-    { path: '/restaurant', component: Restaurant },
-    { path: '/login', component: Login },
-    { path: '/analytics', component: Analytics },
+    { path: '/', component: Home, name: "Home" },
+    { path: '/login', component: Login, name: "Login" },
+    { path: '/restaurant', component: Restaurant, name: "Restaurant" },
+    { path: '/explore', component: Explore, name: "Explore" },
+    { path: '/analytics', component: Analytics, name: "Analytics" },
 ];
 
 const router = createRouter({
