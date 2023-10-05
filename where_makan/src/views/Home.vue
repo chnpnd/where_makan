7fd7d2082c14c2efa1d08b510d014f14a76f27b2
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-05 20:15:16 +0800

    Update Home.vue

--- where_makan/src/views/Home.vue
+++ where_makan/src/views/Home.vue
@@ -2,7 +2,7 @@
 
     <div class="home">
       <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="You did it!!!!" />
     </div>
 
     <div>
