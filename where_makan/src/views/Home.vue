9118e995ac6b3153560974661db138392a74577c
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-06 23:05:01 +0800

    Update Home.vue

--- where_makan/src/views/Home.vue
+++ where_makan/src/views/Home.vue
@@ -3,6 +3,8 @@
     <div class="home">
       <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
       <HelloWorld msg="You did it!" />
+      <h1>Recommend Stalls Near Me</h1>
+      <geolocation-tracker></geolocation-tracker>
     </div>
 
 </template>
