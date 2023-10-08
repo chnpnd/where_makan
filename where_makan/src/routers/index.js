21f61a4c6caa251296d70bf5c50d8e868470ae83
Krystal Lim <61047768+daisyrevolve8383@users.noreply.github.com>
2023-10-08 18:12:10 +0800

    Added Questionnaire
    Questionnaire c1

--- where_makan/src/routers/index.js
+++ where_makan/src/routers/index.js
@@ -6,9 +6,10 @@ import Restaurant from '../views/Restaurant/Restaurant.vue'
 import Login from '../GoogleMap.vue'
 import Analytics from '../views/Analytics.vue';
 import StoreDetails from '../views/Restaurant/StoreDetails.vue';
-import GeolocationTracker from './components/GeolocationTracker.vue';
-import HawkerStall from './views/HawkerStall.vue';
-import StoreDetailsStall from './views/StoreDetailsStall.vue';
+import Questionnaire from '../views/Questionnaire.vue';
+// import GeolocationTracker from './components/GeolocationTracker.vue';
+// import HawkerStall from './views/HawkerStall.vue';
+// import StoreDetailsStall from './views/StoreDetailsStall.vue';
 
 const routes = [
     { path: '/', component: Home },
@@ -17,9 +18,10 @@ const routes = [
     { path: '/store/:storeId', name: 'store-details', component: StoreDetails, props: true }, // Dynamic route for store details
     { path: '/login', component: Login },
     { path: '/analytics', component: Analytics },
-    { path: '/GeolocationTracker', component: GeolocationTracker },
-    { path: '/hawkerstall', component: HawkerStall },
-    { path: '/store/:storeIdStall', name: 'store-details-stall', component: StoreDetailsStall, props: true }, // Dynamic route for store details stall
+    { path: '/questionnaire', component: Questionnaire },
+    // { path: '/GeolocationTracker', component: GeolocationTracker },
+    // { path: '/hawkerstall', component: HawkerStall },
+    // { path: '/store/:storeIdStall', name: 'store-details-stall', component: StoreDetailsStall, props: true }, // Dynamic route for store details stall
 
 ];
 
--- where_makan/src/views/Home.vue
+++ where_makan/src/views/Home.vue
@@ -1,19 +1,33 @@
 <template>
-
-    <div class="home">
-      <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
-      <HelloWorld msg="You did it!" />
-    </div>
-
+  <div class="home">
+    <img alt="Vue logo" class="logo" src="../assets/logo.svg" width="125" height="125" />
+    <HelloWorld msg="You did it!" />
+    <button onclick="launchQuestionnaire()">Reccommend Me What to Eat!</button>
+  </div>
 </template>
 
-<script>
-  import HelloWorld from '@/components/HelloWorld.vue';
-  export default {
-    name: 'Home',
-    components:{
-      HelloWorld
+<script >
+import HelloWorld from '@/components/HelloWorld.vue';
+export default {
+  name: 'Home',
+  inheritAttrs: false,
+  components: {
+    HelloWorld
+  }, methods: {
+    async launchQuestionnaire() {
+      try {
+        router.push({ path: 'Questionnaire' })
+      } catch (error) {
+
+      }
     }
   }
 
+}
+
+
 </script>
+
+
+
+<!-- DO NOT DELETE - KRYSTAL's PART -->
