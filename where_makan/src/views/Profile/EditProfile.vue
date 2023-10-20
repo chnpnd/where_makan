ded3688af877df3068d1d2b7c3b7b3ef075c3907
Swee Long <61006436+ongsweelong@users.noreply.github.com>
2023-10-20 12:22:13 +0800

    New template for edit profile.vue, ideally should be linked to swagger
    Swee Long

---
+++ where_makan/src/views/Profile/EditProfile.vue
@@ -0,0 +1,47 @@
+<template>
+     <form> 
+        <div class="container"> 
+            <h3 class="text-center" style="margin-bottom:10px;"> Edit Your Profile Information </h3> 
+            <div class="row"> 
+                <div class="col form-group"> 
+                   <label class="fw-bold" style="color: purple"> Username </label> 
+                   <input type="text" class="form-control" placeholder="Should reflect from Swagger.io"> 
+                </div>
+
+                <div class="col form-group"> 
+                   <label class="fw-bold" style="color: purple"> Password </label> 
+                   <input type="text" class="form-control" placeholder="Should reflect from Swagger.io"> 
+                </div>
+            </div>
+            
+            <div class="row"> 
+                <div class="col form-group"> 
+                   <label class="fw-bold" style="color: purple"> Date of Birth </label> 
+                   <input type="text" class="form-control" placeholder="Should reflect from Swagger.io"> 
+                </div>
+            
+                <div class="col form-group"> 
+                   <label class="fw-bold" style="color: purple"> Phone Number </label> 
+                   <input type="text" class="form-control" placeholder="Should reflect from Swagger.io"> 
+                </div>
+            </div> 
+
+            <div class = "row"></div>
+            
+            <div class="row"> 
+                <div class = "col"></div>
+                <button type="button" class="btn btn-outline-primary text center col">Save Edits</button>
+                <div class = "col"></div>
+            </div>
+        </div> 
+
+     </form> 
+
+    
+</template>
+
+<script>
+
+//should take the new form values and update them to swagger with the post function
+
+</script>
